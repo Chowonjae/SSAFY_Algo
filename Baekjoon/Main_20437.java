198108c73345d5092142e3c61c082197ea4a3247
Cho <sunmoon1030@gmail.com>
2022-08-31 21:38:18 +0900

    Update Main_20437.java

--- Baekjoon/Main_20437.java
+++ Baekjoon/Main_20437.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-public class BJ_G5_20437_문자열게임2 {
+public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder ans = new StringBuilder();
